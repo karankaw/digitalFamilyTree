b8c275c70be3000f12648dd80643c64a3c862e2b
Karan Kaw <karankaw@hotmail.com>
2018-10-09 21:50:29 +0530

    Changed Regex

--- Solution.java
+++ Solution.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Solution {
-	private static final String INPUT_REGEX_QUERY = "/^Person=[\\w ]+ Relation=(FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)$/gi";
-	private static final String INPUT_REGEX_ADD_EDIT = "/^(FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)=[\\w ]+ (FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)=[\\w ]+$/gi";
+	private static final String INPUT_REGEX_QUERY = "(?i)^Person=[\\w ]+ Relation=(FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)$";
+	private static final String INPUT_REGEX_ADD_EDIT = "(?i)^(FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)=[\\w ]+ (FATHER|MOTHER|BROTHER|SISTER|SON|DAUGHTER|COUSIN|GRANDMOTHER|GRANDFATHER|GRANDSON|GRANDAUGHTER|AUNT|UNCLE)=[\\w ]+$";
 
 	public static void main(String[] args) {
 		
@@ -15,13 +15,19 @@ public class Solution {
 		System.out.println(familyTree.getData());
 
 		String str;
+		System.out.println("Application is started!");
 		Scanner sc = new Scanner(System.in); 
 
 		while(!(str = sc.nextLine()).equalsIgnoreCase("exit")){
 			System.out.println(str);
+			System.out.println(validateInput(str));
 		}
+	}
 
-
-
+	public static boolean validateInput(String str){
+		if(str.matches(INPUT_REGEX_QUERY) || str.matches(INPUT_REGEX_ADD_EDIT)){
+			return true;
+		}
+		return false;
 	}
 }
